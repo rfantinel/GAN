dbda002b1db40d6fa1d2c98388075e9719d88d14
rfantinel <riccardo.fantinel@gmail.com>
2019-09-03 09:15:30 +0200

    change generator

--- main.py
+++ main.py
@@ -56,20 +56,21 @@ class GAN():
         input_layer = Input(shape=noise_shape)
 
         M = Reshape((10, 10, 1))(input_layer)
-        M = Deconvolution2D(filters=64, kernel_size=(3, 3))(M) #64@12x12
-        M = UpSampling2D(size=(2,2))(M) #64@24x24
+        M = Convolution2D(filters=32, kernel_size=(3, 3))(M) #32@8x8
+        M = Convolution2D(filters=16, kernel_size=(3, 3))(M) #16@6x6
+        M = UpSampling2D(size=(3,3))(M) #16@18x18
         M = LeakyReLU(alpha=0.2)(M)
-        M = BatchNormalization(momentum=0.8)(M)
-        M = Convolution2D(filters=32, kernel_size=(3, 3))(M) #32@22x22
-        M = Deconvolution2D(filters=16, kernel_size=(3, 3))(M) #16@24x24
-        M = Deconvolution2D(filters=8, kernel_size=(3, 3))(M) #8@26x26
-        M = LeakyReLU(alpha=0.2)(M)
-        M = BatchNormalization(momentum=0.8)(M)
-        M = Convolution2D(filters=4, kernel_size=(3, 3))(M) #4@24x24
-        M = Deconvolution2D(filters=2, kernel_size=(3, 3))(M) #2@26x26
-        
-        output_layer = Deconvolution2D(filters=1, kernel_size=(3, 3), activation='tanh')(M) #1@28x28
-        
+        M = Convolution2D(filters=8, kernel_size=(3, 3))(M) #8@16x16
+        M = Convolution2D(filters=4, kernel_size=(3, 3))(M) #8@14x14
+        M = UpSampling2D(size=(2,2))(M) #4@28x28
+        M = Flatten() (M)
+        M = Dense(1568, init='normal')(M)
+        M = LeakyReLU(alpha=0.2) (M)
+        M = Dense(784, init='normal')(M)
+               
+        output_layer = Reshape((28, 28, 1))(M)
+    
+       
         model = Model(input_layer, output_layer)
 
         print(model.summary())
@@ -84,18 +85,18 @@ class GAN():
         
         input_layer = Input(shape=img_shape)
 
-        M = Convolution2D(filters=32, kernel_size=(3,3), strides=(1,1))(input_layer) #32@26x26
-        M = MaxPooling2D(pool_size=(2,2))(M) #32@13x13
+        M = Convolution2D(filters=8, kernel_size=(3,3), strides=(1,1))(input_layer) #8@26x26
+        M = MaxPooling2D(pool_size=(2,2))(M) #8@13x13
         
-        M = Convolution2D(filters=64, kernel_size=(3,3), strides=(1,1))(M) #64@11x11
-        M = MaxPooling2D(pool_size=(2,2))(M) #64@5x5
+        M = Convolution2D(filters=16, kernel_size=(3,3), strides=(1,1))(M) #16@11x11
+        M = MaxPooling2D(pool_size=(2,2))(M) #16@5x5
 
-        M = Convolution2D(filters=128, kernel_size=(3,3), strides=(1,1))(M) #128@3x3
+        M = Convolution2D(filters=32, kernel_size=(3,3), strides=(1,1))(M) #32@3x3
 
         M = Flatten() (M)
-        M = Dense(512, init='normal')(M)
+        M = Dense(64, init='normal')(M)
         M = LeakyReLU(alpha=0.2) (M)
-        M = Dense(256, init='normal')(M)
+        M = Dense(128, init='normal')(M)
         M = LeakyReLU(alpha=0.2) (M)
 
         output_layer = Dense(1, init='normal', activation='sigmoid')(M)
@@ -179,4 +180,4 @@ class GAN():
 
 if __name__ == '__main__':
     gan = GAN()
-    gan.train(epochs=30000, batch_size=128, save_interval=200)
+    gan.train(epochs=30000, batch_size=250, save_interval=200)
